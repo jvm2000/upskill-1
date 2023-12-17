2f033b9c6d90fa971c9f0a2f27d004295270bef4
jvm2000 <minozajohnvincent2000@gmail.com>
2023-12-17 22:02:27 +0800

    initial push

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} · LokalTune` : 'LokalTune'
+  },
+  link: [{ rel: 'icon', type: "image/x-icon", href: '/icons/favicon.png' }]
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
